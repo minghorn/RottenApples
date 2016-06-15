59dc7ea851927bdf3f78a3e881425a44479b1e66
Ming Horn <minghaihorn@gmail.com>
2016-06-15 16:06:50 -0700

    Add navigation

---
+++ RottenApples/MovieCollectionCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieCollectionCell.swift
+//  RottenApples
+//
+//  Created by Ming Horn on 6/15/16.
+//  Copyright © 2016 Ming Horn. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionCell: UICollectionViewCell {
+    @IBOutlet weak var MoviePoster: UIImageView!
+    
+}
